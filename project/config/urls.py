319eb83834aba1507206f0feb614635f2f48f6b1
Noelia.DAlessandro <noeliadalessandro@gmail.com>
2023-07-10 13:12:51 -0300

    dejamos de usar funciones y usamos render

--- project/config/urls.py
+++ project/config/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('otra_vista/', views.saludo_vista),
     path('nombre/<nombre>/<apellido>', views.nombre),
     path('probando_template/', views. probando_template), 
+    path('mis_notas/', views.mis_notas),
 ]
--- project/config/views.py
+++ project/config/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import Context, Template 
+from django.shortcuts import render
+#from django.template import Context, Template 
 
 def saludo(request):
 
@@ -14,9 +15,18 @@ def nombre(request, nombre:str , apellido:str):
     return HttpResponse(f"{apellido}, {nombre}")
 
 def probando_template(request):
-	mi_html = open("./templates/template1.html")
-	mi_template = Template(mi_html.read())
-	mi_html.close()
-	mi_contexto = Context()
-	mi_documento = mi_template.render(mi_contexto)
-	return HttpResponse(mi_documento)
+	#mi_html = open("./templates/template1.html")
+	#mi_template = Template(mi_html.read())
+	#mi_html.close()
+	nombre= "Louis"
+	apellido= "Van Beethoven"
+	datos={"nombre": nombre, "apellido": apellido}
+	#mi_contexto = Context(datos)
+	#mi_documento = mi_template.render(mi_contexto)
+	#return HttpResponse(mi_documento)
+	return render (request,"template1.html",datos)
+
+def mis_notas(request):
+    lista_de_notas = [2,6,8,9,7,6,9]
+    contexto = {"notas": lista_de_notas}
+    return render (request, "notas.html", contexto)
