27a47e22c2b155a21fe7b0b473e8da1689ff5597
Noelia.DAlessandro <noeliadalessandro@gmail.com>
2023-07-15 13:06:57 -0300

    acomode app principal home

--- project/apps/home/admin.py
+++ project/apps/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.db import models
+
 
 # Register your models here.
+from .models import Cliente, Pais
+
+admin.site.register(Cliente)
+admin.site.register(Pais)
--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+ 
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50)
+ 
+    def __str__(self):
+        return self.nombre
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nacimiento = models.DateField(null=True)
+    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
